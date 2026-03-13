e1a9bf570e58931c3514e5a18b03fdb84e6a20d9
Simonet Elian BFS <elian.simonet@bfs.admin.ch>
2026-03-13 17:12:15 +0100

    ajout de la page accueil non buguée avec les redirections justes

--- public/account.js
+++ public/account.js
@@ -4,32 +4,32 @@ import { onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/
 
 onAuthStateChanged(auth, async (user) => {
     if (user) {
-
         const docRef = doc(db, "utilisateurs", user.uid);
         const docSnap = await getDoc(docRef);
 
         if (docSnap.exists()) {
             const donnees = docSnap.data();
 
-            document.getElementById("affichage-prenom").innerText = donnees.prenom;
-            document.getElementById("affichage-nom").innerText = donnees.nom;
-            document.getElementById("affichage-age").innerText = donnees.age;
-            document.getElementById("affichage-email").innerText = donnees.email;
+            if(document.getElementById("affichage-prenom")) document.getElementById("affichage-prenom").innerText = donnees.prenom;
+            if(document.getElementById("affichage-age")) document.getElementById("affichage-age").innerText = donnees.age;
+            if(document.getElementById("affichage-email")) document.getElementById("affichage-email").innerText = donnees.email;
 
             const cardImage = document.getElementById("photo-profil-background");
-            
-            if (donnees.photoURL) {
-                cardImage.style.backgroundImage = `url('${donnees.photoURL}')`;
-                cardImage.style.backgroundSize = "cover";
-                cardImage.style.backgroundPosition = "center";
-            } else {
-                cardImage.style.background = "linear-gradient(to bottom, #7c4dff, #2d3436)";
+            if (cardImage) {
+                if (donnees.photoURL) {
+                    cardImage.style.backgroundImage = `url('${donnees.photoURL}')`;
+                    cardImage.style.backgroundSize = "cover";
+                    cardImage.style.backgroundPosition = "center";
+                } else {
+                    cardImage.style.background = "linear-gradient(to bottom, #7c4dff, #2d3436)";
+                }
             }
         }
     } else {
         window.location.href = "login.html";
     }
 });
+
 document.getElementById("btn-deconnexion").addEventListener("click", () => {
     signOut(auth).then(() => {
         window.location.href = "index.html";
